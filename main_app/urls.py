e30296ff1874a6710b258f551210ca8e0a425317
Jassim Hayat <jassim.hayat@gmail.com>
2025-04-18 16:46:43 +0300

    Initial commit

--- main_app/urls.py
+++ main_app/urls.py
@@ -21,7 +21,7 @@ from . import views
 urlpatterns = [
      path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    # path('birds/', views.bird_index, name='bird-index'),
+    path('cats/', views.bird_index, name='cat-index'),
 ]
 
 # path('birds/<int:bird_id>/', views.bird_detail, name='bird-detail'),
