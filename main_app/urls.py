09e89a43ce2fff5337ba12c5ac85f8ad9f6879fc
Jassim Hayat <jassim.hayat@gmail.com>
2025-04-18 19:01:45 +0300

    Update

--- main_app/urls.py
+++ main_app/urls.py
@@ -21,7 +21,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('birds/', views.bird_index, name='cat-index'),
+    path('birds/', views.bird_index, name='bird-index'),
+    path('birds/<int:bird_id>/', views.bird_detail, name='bird-detail'),
 ]
 
-# path('birds/<int:bird_id>/', views.bird_detail, name='bird-detail'),
--- main_app/views.py
+++ main_app/views.py
@@ -1,29 +1,22 @@
 # main_app/views.py
 
 from django.shortcuts import render
+from .models import Bird
 
-# Import HttpResponse to send text-based responses
-from django.http import HttpResponse
+# class Bird:
+#     def __init__(self, name,breed, description, age):
+#         self.name = name
+#         self.breed = breed
+#         self.description = description
+#         self.age = age
 
-
-# views.py
-
-class Bird:
-    def __init__(self, name,breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-# Create a list of Bird instances
-birds = [
-    Bird('Amazon Parrot','baby Amazon Parrot' ,'Chatterbox jungle clown .', 12),
-    Bird('Arican Gray', 'baby Arican Gray','Feathery smartmouth diva.', 10),
-    Bird('Bailey', 'baby Bailey', 'Barky cuddle monster.', 2),
-    Bird('Black Falcon','baby Black Falcon' , 'Sneaky screecher.', 6)
-]
-
-
+# # Create a list of Bird instances
+# birds = [
+#     Bird('Amazon Parrot','baby Amazon Parrot' ,'Chatterbox jungle clown .', 12),
+#     Bird('Arican Gray', 'baby Arican Gray','Feathery smartmouth diva.', 10),
+#     Bird('Bailey', 'baby Bailey', 'Barky cuddle monster.', 2),
+#     Bird('Black Falcon','baby Black Falcon' , 'Sneaky screecher.', 6),
+# ]
 
 def home(request):
     return render(request, 'home.html')
@@ -32,5 +25,11 @@ def about(request):
           return render(request, 'about.html')
 
 def bird_index(request):
- 
+    birds = Bird.objects.all()  # look familiar?
     return render(request, 'birds/index.html', {'birds': birds})
+
+
+def bird_detail(request, bird_id):
+    bird = Bird.objects.get(id=bird_id)
+    return render(request, 'birds/detail.html', {'bird': bird})
+
